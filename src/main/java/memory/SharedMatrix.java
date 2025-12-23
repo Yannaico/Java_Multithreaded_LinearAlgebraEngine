809a97e5cd15f97101de26dbadebaf9463fdde91
yonathanZel <zelnick@post.bgu.ac.il>
2025-12-23 19:58:51 +0200

    SharedVector & SharedMatrix

--- src/main/java/memory/SharedMatrix.java
+++ src/main/java/memory/SharedMatrix.java
@@ -5,54 +5,110 @@ public class SharedMatrix {
     private volatile SharedVector[] vectors = {}; // underlying vectors
 
     public SharedMatrix() {
-        // TODO: initialize empty matrix
+        this.vectors = new SharedVector[0];
     }
 
     public SharedMatrix(double[][] matrix) {
-        // TODO: construct matrix as row-major SharedVectors
+        this.vectors = new SharedVector[matrix.length];
+        loadRowMajor(matrix);
     }
 
     public void loadRowMajor(double[][] matrix) {
-        // TODO: replace internal data with new row-major matrix
+        if(matrix == null || matrix.length == 0){
+            this.vectors = new SharedVector[0];
+            return;
+        }
+        int numRows = matrix.length;
+        SharedVector[] newVectors = new SharedVector[numRows];
+        for (int i = 0; i < numRows; i++) {
+            newVectors[i] = new SharedVector(matrix[i], VectorOrientation.ROW_MAJOR);
+        }
+        this.vectors = newVectors;
     }
 
     public void loadColumnMajor(double[][] matrix) {
-        // TODO: replace internal data with new column-major matrix
+        if(matrix == null || matrix.length == 0){
+            this.vectors = new SharedVector[0];
+            return;
+        }
+
+        int numRows = matrix.length;
+        int numCols = matrix[0].length;
+
+        SharedVector[] newVectors = new SharedVector[numCols];
+        for(int i=0;i< numCols;i++){
+            double[] tempColVector = new double[numRows];
+            for(int j=0;j< numRows;j++){
+                tempColVector[j]=matrix[j][i];
+            }
+            newVectors[i] = new SharedVector(tempColVector, VectorOrientation.COLUMN_MAJOR);
+        }
+        this.vectors = newVectors;
     }
 
     public double[][] readRowMajor() {
         // TODO: return matrix contents as a row-major double[][]
-        return null;
+        int numRows = this.vectors.length;
+        if(numRows == 0){
+            return new double[0][0];
+        }
+        acquireAllVectorReadLocks(vectors);
+        double[][] result;
+        try{
+            int numCols = this.vectors[0].length();
+            result = new double[numRows][numCols];
+            for(int i=0;i<numRows;i++){
+                for(int j=0;j<numCols;j++){
+                    result[i][j] = this.vectors[i].get(j);
+                }
+            }
+        }
+        finally{
+            releaseAllVectorReadLocks(vectors);
+        }
+        return result;
     }
 
     public SharedVector get(int index) {
-        // TODO: return vector at index
-        return null;
+        return this.vectors[index];
     }
 
     public int length() {
-        // TODO: return number of stored vectors
-        return 0;
+        return this.vectors.length;
     }
 
     public VectorOrientation getOrientation() {
-        // TODO: return orientation
-        return null;
+        if(this.vectors != null || this.length() == 0)
+            throw new IllegalArgumentException();
+        return this.vectors[0].getOrientation();
     }
 
     private void acquireAllVectorReadLocks(SharedVector[] vecs) {
-        // TODO: acquire read lock for each vector
+        for(int i=0;i<vecs.length;i++)
+        {
+            vecs[i].readLock();
+        }
     }
 
     private void releaseAllVectorReadLocks(SharedVector[] vecs) {
-        // TODO: release read locks
+         for(int i=0;i<vecs.length;i++)
+        {
+            vecs[i].readUnlock();
+        }
     }
 
     private void acquireAllVectorWriteLocks(SharedVector[] vecs) {
-        // TODO: acquire write lock for each vector
+         for(int i=0;i<vecs.length;i++)
+        {
+            vecs[i].writeLock();
+        }
     }
 
     private void releaseAllVectorWriteLocks(SharedVector[] vecs) {
-        // TODO: release write locks
+        
+         for(int i=0;i<vecs.length;i++)
+        {
+            vecs[i].writeLock();
+        }
     }
 }
--- src/main/java/memory/SharedVector.java
+++ src/main/java/memory/SharedVector.java
@@ -20,7 +20,7 @@ public class SharedVector {
             return vector[index];
         }
         finally{
-            readLock();
+            readUnlock();
         }
     }
 
@@ -48,19 +48,14 @@ public class SharedVector {
         this.lock.readLock().unlock();
     }
 
+    //Assumes caller holds write lock.
     public void transpose() {
-        writeLock();
-        try{
-            if(this.orientation == VectorOrientation.ROW_MAJOR)
-                this.orientation = VectorOrientation.COLUMN_MAJOR;
-            else
-                this.orientation = VectorOrientation.ROW_MAJOR;
-        }
-        finally{
-            writeUnlock();
-        }
-    }
-
+    if(this.orientation == VectorOrientation.ROW_MAJOR)
+        this.orientation = VectorOrientation.COLUMN_MAJOR;
+    else
+        this.orientation = VectorOrientation.ROW_MAJOR;
+}
+    //Assumes caller holds write lock on this and read lock on other.
     public void add(SharedVector other) {
         if (this.orientation != other.getOrientation()) {
             throw new IllegalArgumentException("Cannot add vectors with different orientations");
@@ -69,42 +64,22 @@ public class SharedVector {
              throw new IllegalArgumentException("Vector lengths do not match");
         }
         
-        writeLock();
-        try{
-            other.readLock();
-            try{
-                for(int i =0;i<vector.length;i++)
-                {
-                    vector[i]+=other.get(i);
-                }
-            }
-            finally{
-                other.readUnlock();
-            }
-        }
-        finally{
-            writeUnlock();
+        other.readLock();
+        for(int i=0;i<vector.length;i++){
+            this.vector[i]+=other.get(i); 
         }
     }
 
     public void negate() {
-        writeLock();
-        try{
-            for(int i =0;i<vector.length;i++)
-            {
-                vector[i]*=-1;
-            }
-        }
-        finally{
-            writeUnlock();
+        for(int i=0;i<vector.length;i++)
+        {
+            vector[i]*=-1;
         }
     }
 
+    //Assumes caller holds read locks on both vectors.
     public double dot(SharedVector other) {
-        
-
         double sum=0;
-
 
         if (this.orientation == other.getOrientation()) 
         {
@@ -114,42 +89,45 @@ public class SharedVector {
         {
              throw new IllegalArgumentException("Vector lengths do not match");
         }
-        
-        for(int i=0;i<this.vector.length;i++)
-        {
-            sum+=this.get(i) * other.get(i);
 
-
+        for (int i = 0; i < this.vector.length; i++) {
+            sum += this.vector[i] * other.vector[i];
         }
         return sum;
     }
 
     public void vecMatMul(SharedMatrix matrix) {
-        if (this.orientation == matrix.getOrientation()) 
-        {
-            throw new IllegalArgumentException("Cannot Multiply vectors with same orientations");
-        }
-        if(matrix.length()==0 || this.length()!= matrix.get(0).length())
-        {
-              throw new IllegalArgumentException("Matrix length is not compatible with the vector");
+        /*
+        [x y z]    [a b c] [a]
+        [a b c]            [b]
+        [d e f]            [c]
 
-        }
-
-        writeLock();
-
-        try{
-            matrix.acquireAllVectorReadLocks();
-            for(int i=0 ;i< matrix.length();i++)
-            {
-                SharedVector current = matrix.get(i);
-                vector[i] = dot(current);
-            }
-        }
-        finally{
-            matrix.r
-        }
         
+        
+        */
+        if (this.orientation != VectorOrientation.ROW_MAJOR) {
+            throw new IllegalArgumentException("Vector must be in ROW_MAJOR orientation for vec-mat multiplication");
+        }
 
+        int matrixRows = matrix.length();
 
+        if (matrixRows == 0) {
+            throw new IllegalArgumentException("Matrix is empty");
+        }
+        if(vector.length != matrixRows) {
+            throw new IllegalArgumentException("Vector length must match matrix row count");
+        }
+
+        int matrixCols = matrix.get(0).length();
+        double[] result = new double[matrixCols];
+
+        for(int i=0;i<matrixCols;i++){
+            double sum=0;
+            for(int j=0;j<matrixRows;j++){
+                sum+= this.vector[j] * matrix.get(j).get(i);
+            }
+            result[i]=sum;
+        }
+        this.vector = result;
     }
 }
